258b9754973c15c2feed00c439526d6f5c43ba0d
Aphitchaya <Aphitchaya.l@ku.th>
2026-02-27 21:03:12 +0700

    when you clik it will turn

---
+++ Assets/Script/PlayerInput.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public Transform model;
+
+    public bool CanMove { get; private set; }
+    public bool MoveLeft { get; private set; }
+    public bool MoveRight { get; private set; }
+
+    private bool hasTurned = false;
+    private PlayerMovement movement;
+    private PlayerRise rise;
+
+    void Awake()
+    {
+        movement = GetComponent<PlayerMovement>();
+        rise = GetComponent<PlayerRise>();
+    }
+
+    void Update()
+    {
+        if (rise.IsRising)
+            return;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (!hasTurned)
+            {
+                model.localRotation = Quaternion.Euler(0f, 0f, 0f);
+                hasTurned = true;
+            }
+
+            CanMove = true;
+        }
+
+        if (!CanMove) return;
+
+        MoveLeft = Input.GetKey(KeyCode.A);
+        MoveRight = Input.GetKey(KeyCode.D);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            movement.Jump();
+        }
+    }
+}
+
+//🎯 วิธีใช้ (สำคัญมาก)
+
+//ใส่ 3 Script นี้ลง GameObject Player ตัวเดียวกัน
+
+//ต้องมี
+
+//Rigidbody
+
+//Animator
+
+//ตั้ง Tag พื้นเป็น "Ground"
+
+//ลาก Model ไปใส่ในช่อง model ของ PlayerInput
--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -1,30 +1,73 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 1f;
     public float horizontalSpeed = 3f;
-    
-    
-    void Update()
+    public float jumpForce = 5f;
+    public float fallMultiplier = 2.5f;
 
+    private Rigidbody rb;
+    private Animator animator;
+    private bool isGrounded;
+
+    private PlayerRise rise;
+    private PlayerInput input;
+
+    void Awake()
     {
-        bool isTurning = false;
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponentInChildren<Animator>();
+        rise = GetComponent<PlayerRise>();
+        input = GetComponent<PlayerInput>();
+    }
 
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
-        if (Input.GetKey(KeyCode.D))
+    void FixedUpdate()
+    {
+        if (rise.IsRising || !input.CanMove) return;
+
+        Vector3 move = Vector3.right * speed;
+
+        if (input.MoveRight)
+            move += Vector3.back * horizontalSpeed;
+
+        if (input.MoveLeft)
+            move += Vector3.forward * horizontalSpeed;
+
+        rb.linearVelocity = new Vector3(move.x, rb.linearVelocity.y, move.z);
+
+        if (rb.linearVelocity.y < 0)
         {
-            
-            transform.Translate(Vector3.right * horizontalSpeed * Time.deltaTime);
-           
+            rb.linearVelocity += Vector3.up * Physics.gravity.y *
+                                 (fallMultiplier - 1) *
+                                 Time.fixedDeltaTime;
         }
-        if (Input.GetKey(KeyCode.A))
-        {
-           
-            transform.Translate(Vector3.left * horizontalSpeed * Time.deltaTime  );
-           
-            }
-     
+    }
 
+    public void Jump()
+    {
+        if (!isGrounded) return;
+
+        rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        animator.SetBool("isJumping", true);
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+            animator.SetBool("isJumping", false);
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
     }
 }
